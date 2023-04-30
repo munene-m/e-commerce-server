03877b6e3ff5cbda2be584c1e0c123c833f901e1
munene-m <munenenjue18@gmail.com>
2023-04-30 20:20:00 +0300

    fixed env variables

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,8 @@ import { v2 as cloudinary } from 'cloudinary'
 import asyncHandler from "express-async-handler"
 import products from "../models/products.js"
 import multer from 'multer';
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 const storage = multer.diskStorage({
@@ -16,9 +18,9 @@ const storage = multer.diskStorage({
   const upload = multer({ storage });
 
 cloudinary.config({
-    cloud_name: "doj335om4",
-    api_key: 522876894532472,
-    api_secret: "pZQf_dO0emYz0n5nzjdw38lpk4M"
+    cloud_name: process.env.CLOUDNAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 //Create product
@@ -43,7 +45,8 @@ export const createProduct = asyncHandler( async(req, res) =>{
             description: product.description,
             price: product.price,
             quantity: product.quantity,
-            image: product.image
+            image: product.image,
+            imagePublicId: product.imagePublicId
         })
     } else {
         res.status(400).json({error: "An error occurred when creating the product"})
@@ -56,9 +59,25 @@ export const updateProduct = asyncHandler(async (req,res) =>{
     if(!product){
         res.status(400)
         throw new Error("The product you tried to update does not exist")
-    }
-    const updatedProduct = await products.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(200).json(updatedProduct);
+      }
+      const { name, description, quantity, price } = req.body
+      let image = product.image
+      let imagePublicId = product.imagePublicId
+      if (req.file) {
+        // If a new image is uploaded, update it in Cloudinary
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          width: 500,
+          height: 500,
+          crop: "fill",
+        });
+        image = result.secure_url
+        imagePublicId = result.public_id
+      }
+      const updatedProduct = await products.findByIdAndUpdate(req.params.id,
+        { name, description, quantity, price, image, imagePublicId },
+        { new: true }
+      )
+      res.status(200).json(updatedProduct);
 })
 
 export const getProducts = asyncHandler( async(req, res) =>{
@@ -77,7 +96,7 @@ export const deleteProduct = asyncHandler(async(req, res) => {
         res.status(404);
         throw new Error("Product not found ");
     }
-    await service.remove();
+    await products.findByIdAndDelete();
     res.status(200).json({ id: req.params.id })
 })
 
