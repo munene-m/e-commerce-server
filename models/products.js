2c4f38655033576e42b81e5da4d1e0f9d9322057
munene-m <munenenjue18@gmail.com>
2023-04-30 18:45:28 +0300

    changed image required status

--- models/products.js
+++ models/products.js
@@ -4,19 +4,21 @@ const productSchema = mongoose.Schema({
     name:{
         type: String,
         required: true,
-        unique: true
     },
     description: {
         type:String,
         required: true 
     },
     price: {
+        type: String,
+        required: true
+    },
+    quantity: {
         type: Number,
         required: true
     },
     image: {
-        type: String,
-        required: true
+        type: String
     }
 })
 
