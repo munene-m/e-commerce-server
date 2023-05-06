e70b577f8238108f7bf66a1183be85f22ac7de24
munene-m <munenenjue18@gmail.com>
2023-05-06 20:34:21 +0300

    created cart and payment controller

---
+++ controllers/cartController.js
@@ -0,0 +1,61 @@
+import cart from "../models/cart.js";
+import auth from "../models/auth.js";
+import asyncHandler from "express-async-handler"
+
+export const addProduct = asyncHandler(async(req,res) => {
+    const { productId, name, image, price, quantity } = req.body
+    if(!productId || !name || !image || !price || !quantity){
+        res.status(400)
+        throw new Error("Please enter all required fields")
+    }
+    const user = auth.find(req.user.id)
+    if(!user) {
+        res.status(404);
+        throw new Error("User not found");
+    }
+    const cartItems = await cart.create({
+        productId, name, image, price, quantity
+    })
+    if(cartItems) {
+        res.status(200).json({
+            _id: cartItems.id,
+            productId: cartItems.productId,
+            name: cartItems.name,
+            price: cartItems.price,
+            quantity: cartItems.quantity,
+            image: cartItems.image
+        })
+    } else {
+        res.status(400).json({error: "An error occurred when adding product to cart"})
+    }
+})
+
+export const updateProduct = asyncHandler(async(req, res) => {
+    const cartItem = await cart.findById(req.params.id)
+    if(!cartItem) {
+        res.status(404);
+        throw new Error("Product does not exist");
+    }
+    const updatedProduct = await cart.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedProduct);
+})
+
+export const getCart = asyncHandler(async(req,res) => {
+    const cartItems = await cart.find()
+    if(!cartItems){
+        res.status(400)
+        throw new Error("There are no items in cart")
+    }
+    res.status(200).json(cartItems)
+})
+export const deleteProduct = asyncHandler( async(req,res) => {
+    const cartItem = await cart.findById(req.params.id)
+    if(!cartItem) {
+        res.status(400).json("Product not found")
+    }
+    await cart.findByIdAndDelete(req.params.id)
+    res.status(200).json({id: req.params.id, message: "Item deleted"})
+
+} )
+
+export default { addProduct, updateProduct,getCart, deleteProduct }
