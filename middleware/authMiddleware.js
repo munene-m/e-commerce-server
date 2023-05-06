7a81009e89a02c2132ce12bcb313de9a83a9a796
munene-m <munenenjue18@gmail.com>
2023-05-06 20:36:08 +0300

    formatting changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,24 +1,23 @@
-import asyncHandler from "express-async-handler"
-import jwt from "jsonwebtoken"
-import authModel from "../models/auth.js"
-export const protect = asyncHandler( async (req, res, next) => {
-    let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        token = req.headers.authorization.split(' ')[1]
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import authModel from "../models/auth.js";
+export const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if ( req.headers.authorization && req.headers.authorization.startsWith("Bearer") ) {
+    token = req.headers.authorization.split(" ")[1];
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = await authModel.findById(decoded.id).select('-password')
+    req.user = await authModel.findById(decoded.id).select("-password");
 
-        next()
-    } else {
-        res.status(401)
-        throw new Error("Unauthorized attempt")
-    }
-    if(!token){
-        res.status(401)
-        throw new Error("No authorization without token")
-    }
-
-})
-export default {protect}
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Unauthorized attempt");
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("No authorization without token");
+  }
+});
+export default { protect };
