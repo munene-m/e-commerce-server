0c51b36bd3979e803be6f0023ee49c4223d347cf
munene-m <munenenjue18@gmail.com>
2023-05-06 20:37:35 +0300

    added new route to product routes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import multer from 'multer'
 import { protect } from '../middleware/authMiddleware.js'
-import { createProduct, updateProduct, deleteProduct, getProducts, getProductInCategory } from '../controllers/productController.js'
+import { createProduct, updateProduct, deleteProduct, getProduct, getProducts, getProductInCategory } from '../controllers/productController.js'
 const upload = multer({ dest: 'uploads/'})
 const router = express.Router()
 
 router.route("/").get(protect, getProducts)
 router.route("/create").post(protect, upload.single('image'), createProduct)
 router.route("/update/:id").put(protect, upload.single('image'), updateProduct)
+router.route("/product/:id").get(protect, getProduct)
 router.route("/product/:value").get(protect, getProductInCategory)
 router.route("/delete/:id").delete(protect, deleteProduct)
 
