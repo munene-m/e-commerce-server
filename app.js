c15626840e6f328d10acdf93f7b65aae09acc960
munene-m <munenenjue18@gmail.com>
2023-05-10 21:21:20 +0300

    finsl commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import { connectDB } from "./config/db.js"
 import authRoute from './routes/authRoutes.js'
 import productRoute from './routes/productRoute.js'
@@ -22,6 +24,16 @@ app.use("/auth", authRoute)
 app.use("/products", productRoute)
 app.use("/cart", cartRoute)
 app.use("/pay", paymentRoute)
+
+// Serve static files from the 'dist' directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Handle all other routes and return the 'index.html' file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
