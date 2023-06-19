281aecdcbde2b4f885ab5b6e473d0b14a1c9c4e6
munene-m <munenenjue18@gmail.com>
2023-06-19 08:02:32 +0300

    update password endpoint fixed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 const router = express.Router()
 import { protect } from '../middleware/authMiddleware.js'
-import { registerUser, loginUser, getCredentials, updateUser, forgotPassword, googleAuthCallback, resetPassword } from '../controllers/authController.js'
+import { registerUser, loginUser, getCredentials, updatePassword, forgotPassword, resetPassword } from '../controllers/authController.js'
 router.route("/credentials").get(protect, getCredentials)
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
-router.route("/update/:id").put(updateUser)
+router.route("/update").upt(updatePassword)
 router.route("/forgot-password").put(forgotPassword)
 router.route("/reset").post(resetPassword)
-router.route("/oauth/callback").get(googleAuthCallback)
+// router.route("/oauth/callback").get(googleAuthCallback)
 
 export default router
